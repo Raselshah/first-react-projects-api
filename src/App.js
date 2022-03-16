efa6c6390b6f2122e675f6e5c7b9324e621477c6
Md Rasel Shah <mdraselshah56@gmail.com>
2022-03-16 22:20:59 +0600

    data loaded and set ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <LoadCountries></LoadCountries>
+      <SetAllCountry></SetAllCountry>
     </div>
   );
 }
 
 function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
       <h2>Free all countries tour</h2>
+      <h3>country {countries.length}</h3>
+
+      {countries.map((country) => (
+        <SetAllCountry
+          name={country.name.common}
+          area={country.area}
+          border={country.borders + " "}
+          capital={country.capital + " "}
+          photo={country.flags.png}
+          region={country.region}
+        ></SetAllCountry>
+      ))}
+    </div>
+  );
+}
+
+function SetAllCountry(props) {
+  return (
+    <div className="container">
+      <div className="country-info">
+        <h2>Country : {props.name}</h2>
+        <h3>Area : {props.area}</h3>
+        <h3>Area : {props.border}</h3>
+        <h3>capital : {props.capital}</h3>
+        <h3>region : {props.region}</h3>
+        <img className="img-size" src={props.photo} alt="" />
+      </div>
     </div>
   );
 }
